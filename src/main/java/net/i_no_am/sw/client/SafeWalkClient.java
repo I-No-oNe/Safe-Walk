3189088a12b57c744e8e3df1d82ed32b1b2e35fb
I-No-oNe <novonoam76@gmail.com>
2024-06-30 19:36:58 +0300

    sort some stuff

---
+++ src/main/java/net/i_no_am/sw/client/SafeWalkClient.java
@@ -0,0 +1,9 @@
+package net.i_no_am.sw.client;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SafeWalkClient implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
